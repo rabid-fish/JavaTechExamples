b3efefb0d3130456693150694469a1770d956c54
rabid-fish <spam@danandlaurajuliano.com>
2014-03-01 16:08:43 -0600

    Updates to ExampleRefit row fixture tests to better explain how this fixture type is used

--- ExampleRefit/src/main/java/com/github/rabid_fish/MathOperationRowFixture.java
+++ ExampleRefit/src/main/java/com/github/rabid_fish/MathOperationRowFixture.java
@@ -2,16 +2,17 @@ package com.github.rabid_fish;
 
 public class MathOperationRowFixture extends fit.RowFixture {
 
+	MathOperation mathOperation = new MathOperation();
+	
 	public static class MathOperationRow {
 
 		private int arg1;
 		private int arg2;
 		private int result;
 		
-		public MathOperationRow(int arg1, int arg2, int result) {
+		public MathOperationRow(int arg1, int arg2) {
 			this.arg1 = arg1;
 			this.arg2 = arg2;
-			this.result = result;
 		}
 		
 		public int getArg1() {
@@ -25,6 +26,10 @@ public class MathOperationRowFixture extends fit.RowFixture {
 		public int getResult() {
 			return result;
 		}
+
+		public void callClassUnderTest(MathOperation mathOperation) {
+			this.result = mathOperation.add(arg1, arg2);
+		}
 	}
 
 	public Class<?> getTargetClass() {
@@ -34,9 +39,13 @@ public class MathOperationRowFixture extends fit.RowFixture {
 	@Override
 	public Object[] query() throws Exception {
 		
-		MathOperationRow row1 = new MathOperationRow(4, 4, 8);
-		MathOperationRow row2 = new MathOperationRow(3, 5, 8);
-		MathOperationRow row3 = new MathOperationRow(6, 9, 15);
+		MathOperationRow row1 = new MathOperationRow(4, 4);
+		MathOperationRow row2 = new MathOperationRow(3, 5);
+		MathOperationRow row3 = new MathOperationRow(6, 9);
+		
+		row1.callClassUnderTest(mathOperation);
+		row2.callClassUnderTest(mathOperation);
+		row3.callClassUnderTest(mathOperation);
 		
 		return new MathOperationRow[] { row1, row2, row3 };
 	}
