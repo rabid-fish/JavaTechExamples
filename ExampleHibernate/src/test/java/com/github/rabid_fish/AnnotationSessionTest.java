a9e45e5ca33294134436e0aea83862fcde3cc31d
Daniel Juliano <spam@danandlaurajuliano.com>
2014-08-13 22:13:00 -0500

    ExampleHibernate - fixed bug with test method order

--- ExampleHibernate/src/test/java/com/github/rabid_fish/AnnotationSessionTest.java
+++ ExampleHibernate/src/test/java/com/github/rabid_fish/AnnotationSessionTest.java
@@ -1,12 +1,14 @@
 package com.github.rabid_fish;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:/META-INF/spring-annotationsession-context.xml")
@@ -16,6 +18,7 @@ public class AnnotationSessionTest {
 	UserDao repository;
 
 	@Test
+	@Transactional
 	public void testSave() {
 
 		User user = new User();
@@ -23,10 +26,6 @@ public class AnnotationSessionTest {
 		repository.save(user);
 
 		assertEquals(user, repository.findOne(user.getId()));
-	}
-	
-	@Test
-	public void testFindByName() {
 		
 		assertTrue(repository.findByName("Dan") != null);
 	}
